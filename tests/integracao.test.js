383deecf23392fc1e294e5cde914227231a1b2f7
Kel Leao <raquel179@gmail.com>
2024-05-29 22:37:23 -0300

    Adcionar github

---
+++ tests/integracao.test.js
@@ -0,0 +1,22 @@
+const { getEnderecoCep } = require("../src/cep");
+
+describe("Teste o endereço do CEP", () => {
+  test("Válida CEP", async () => {
+    const data = await getEnderecoCep("31515-220");
+    expect(data).toHaveProperty("cep", "31515-220");
+    expect(data).toHaveProperty("logradouro", "Rua José Sanguinete");
+    expect(data).toHaveProperty("complemento", "");
+    expect(data).toHaveProperty("bairro", "São João Batista (Venda Nova)");
+    expect(data).toHaveProperty("localidade", "Belo Horizonte");
+    expect(data).toHaveProperty("uf", "MG");
+    expect(data).toHaveProperty("ibge", "3106200");
+    expect(data).toHaveProperty("ddd", "31");
+  });
+
+  
+  test("CEP inválido", async () => {
+    await expect(getEnderecoCep("31515220")).rejects.toThrow(
+      "Inválido"
+    );
+  });
+});
